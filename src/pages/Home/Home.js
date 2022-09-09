8d6b0cbce2c9482518ee0c07ad77134c9a1282ca
Ramazan USLU <ruslu1720@gmail.com>
2022-09-09 20:49:42 +0300

    Filter islemleri tamamlandi

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import Search from "../../components/Home/Search/Search";
 import Filter from "../../components/Home/Filter/Filter";
@@ -10,7 +10,8 @@ const Home = () => {
   const [selectedRating, setSelectedRating] = useState(null);
   const [selectedPrice, setSelectedPrice] = useState([0, 500]);
   const [brands, setBrands] = useState(brand);
-  const [list, setList] = useState(productsList)
+  const [list, setList] = useState(productsList);
+  const [inputSearch, setInputSearch] = useState("");
 
   const handleSelectCategory = (event, value) =>
     // if not select category than show all or selected category => ? == if, === or
@@ -29,9 +30,70 @@ const Home = () => {
   const handleChangePrice = (event, value) => {
     setSelectedPrice(value);
   };
+  //-----------------------------------------------------------------
+
+  // Filtering Products
+
+  const applyFilters = () => {
+    let updateProductList = productsList;
+
+    // Rating Filters
+    if (selectedRating) {
+      updateProductList = updateProductList.filter(
+        (item) => parseInt(item.rating) === parseInt(selectedRating)
+      );
+    }
+
+    //-----------------------------------------------------
+    // Category Filters
+    if (selectedCategory) {
+      updateProductList = updateProductList.filter(
+        (item) => item.category.toUpperCase() === selectedCategory.toUpperCase()
+      );
+    }
+
+    //-----------------------------------------------------
+    // Brand Filters
+    const brandChecked = brands
+      .filter((item) => item.checked)
+      .map((item) => item.label.toLowerCase());
+    if (brandChecked.length) {
+      updateProductList = updateProductList.filter((item) =>
+        brandChecked.includes(item.brand.toLowerCase())
+      );
+    }
+
+    //-----------------------------------------------------
+    // Price Filters
+    const minPrice = selectedPrice[0];
+    const maxPrice = selectedPrice[1];
+    updateProductList = updateProductList.filter(
+      (item) => item.price >= minPrice && item.price <= maxPrice
+    );
+
+    //-----------------------------------------------------
+    // Search Filters
+    if (inputSearch) {
+      updateProductList = updateProductList.filter(
+        (item) =>
+          item.title.toLowerCase().search(inputSearch.toLowerCase().trim()) !==
+          -1
+      );
+    }
+
+    setList(updateProductList);
+  };
+
+  useEffect(() => {
+    applyFilters();
+  }, [selectedRating, selectedCategory, brands, selectedPrice, inputSearch]);
+
   return (
     <div className="h-container">
-      <Search />
+      <Search
+        value={inputSearch}
+        changeInput={(e) => setInputSearch(e.target.value)}
+      />
       <div className="h-row">
         <div className="h-col">
           <Filter
@@ -46,7 +108,7 @@ const Home = () => {
           />
         </div>
         <div className="h-col">
-          <List list={list}/>
+          <List list={list} />
         </div>
       </div>
     </div>
